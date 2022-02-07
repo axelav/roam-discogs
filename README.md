5d8980e00a5d69ac89d3f27f2c0191d519a118c1
axelav <axel@ftml.net>
2022-02-07 12:58:03 -0500

    chore: add README

---
+++ README.md
@@ -0,0 +1,20 @@
+# Roam Research `{{Discogs Search}}`
+
+## Installation
+
+- Create a new block in your graph with the text `{{[[roam/js]]}}`.
+- Click "Yes, I know what I'm doing".
+- Create a new block as a child.
+- Type a backslash (`/`), then find and select the "Javascript Code Block" option.
+- Paste the contents of this file into the child block.
+- Get an API token from Discogs: https://www.discogs.com/settings/developers
+- Replace your token in the code (line 22).
+
+## Usage
+
+- You can now create blocks with the text `{{Discogs Search}} <search terms here>`
+- A button will appear. Click it and the release details will be written to your Roam graph.
+
+## TODO
+
+- better message to user when no results; only logs to console currently
--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-// create click handler for Discogs Sync button
+// # Roam Research `{{Discogs Search}}`
+//
+// ## Installation
+//
+// - Create a new block in your graph with the text `{{[[roam/js]]}}`.
+// - Click "Yes, I know what I'm doing".
+// - Create a new block as a child.
+// - Type a backslash (`/`), then find and select the "Javascript Code Block" option.
+// - Paste the contents of this file into the child block.
+// - Get an API token from Discogs: https://www.discogs.com/settings/developers
+// - Replace your token in the code (line 22).
+//
+// ## Usage
+//
+// - You can now create blocks with the text `{{Discogs Search}} <search terms here>`
+// - A button will appear. Click it and the release details will be written to your Roam graph.
+//
+// ## TODO
+//
+// - better message to user when no results; only logs to console currently
+
+const token = 'YOUR_DISCOGS_TOKEN_HERE'
+
 if (window.discogsBtn) {
   document.removeEventListener(discogsBtn.handleClick)
 } else {
@@ -34,9 +56,6 @@ discogsBtn.handleClick = async (e) => {
 }
 
 document.addEventListener('click', discogsBtn.handleClick, false)
-
-//place Discogs v1 api token here
-const token = 'YOUR-TOKEN-HERE'
 
 const handleResponse = async (res) => {
   if (!res.ok) {
@@ -81,7 +100,6 @@ const writeTag = (str) => {
   }
 }
 
-//create new block and place data
 const addRelease = (release, page_uid) => {
   const { artists, title, year, genres, styles, label } = release
   const tags = [...genres, ...styles]
@@ -126,9 +144,9 @@ const addData = async (query, uid) => {
         uid
       )
     } else {
-      console.warn('No results!')
+      console.log('Discogs Search :: No Results')
     }
   } catch (err) {
-    console.error('Discogs Search Error: ', err)
+    console.error('Discogs Search :: Error', err)
   }
 }
